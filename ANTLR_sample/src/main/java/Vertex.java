7cae2a2cc98d5828414b0b8dec5b3e987f85faec
ray <lenny419@163.com>
2020-01-30 14:52:56 +0800

    first java repo submit

---
+++ ANTLR_sample/src/main/java/Vertex.java
@@ -0,0 +1,19 @@
+public class Vertex implements Comparable<Vertex> {
+    private String label;
+
+    public Vertex(String label) {
+        this.label = label.toUpperCase();
+    }
+
+    public int compareTo(Vertex o) {
+        return (this.getLabel().compareTo(o.getLabel()));
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
