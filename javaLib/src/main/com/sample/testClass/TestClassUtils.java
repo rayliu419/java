96860612feae97613520437312bbb4250b77dbcb
ray <lenny419@163.com>
2020-03-30 22:16:08 +0800

    add comments

--- javaLib/src/main/com/sample/testClass/TestClassUtils.java
+++ javaLib/src/main/com/sample/testClass/TestClassUtils.java
@@ -2,8 +2,14 @@ package com.sample.testClass;
 
 import com.data.BaseClass;
 import com.data.ClassExtendsBase;
-import com.sample.teststatic.B;
 
+/**
+ * class.forName()
+ * ClassEntendsBase.class
+ * Class<T> 的用法
+ * https://www.cnblogs.com/sharpest/p/7807009.html
+ *
+ */
 public class TestClassUtils {
     public static void main(String[] args) {
         BaseClass c = new ClassExtendsBase();
