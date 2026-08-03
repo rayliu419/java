eff234f0c4fc4315c7895852cedeb3375eb11dcf
rayliu419 <lenny419@163.com>
2026-08-03 23:43:26 +0800

    update

--- algorithm/src/main/java/leetcode/Exam.java
+++ algorithm/src/main/java/leetcode/Exam.java
@@ -3,4 +3,23 @@ package leetcode;
 
 public class Exam {
 
+    int mySqrt(int x) {
+        if (x < 2) return x;
+        int low = 1;
+        int high = 46340;
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (mid == x / mid) {
+                return mid;
+            } else if (mid > x / mid ) {
+                high = mid - 1;
+            } else {
+                // 可能也是一个解
+                ans = mid;
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
 }
