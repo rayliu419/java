c7a5ca09ed949f1457f21b8523f05ffc03e08ff0
rayliu419 <lenny419@163.com>
2026-08-06 21:38:37 +0800

    refine toc

--- algorithm/src/main/java/leetcode/Exam.java
+++ algorithm/src/main/java/leetcode/Exam.java
@@ -4,13 +4,5 @@ package leetcode;
 import infra.TreeNode;
 
 public class Exam {
-
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q != null) return false;
-        if (p != null && q == null) return false;
-        if (p == null && q == null) return true;
-        if (p.val != q.val) return false;
-        if (!isSameTree(p.left, q.left)) return false;
-        return isSameTree(p.right, q.right);
-    }
+    
 }
